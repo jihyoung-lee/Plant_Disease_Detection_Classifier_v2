cc72cba3cf0581419bc607e553b08d15e1ba7444
wlgud <wlgud3412@naver.com>
2026-07-04 13:45:57 +0900

    refactor: run prediction service in threadpool

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, UploadFile, Form
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.concurrency import run_in_threadpool
 from app.service.prediction_service import PredictionService
 from app.schemas.prediction import PredictionResponse, PredictionData
 from app.exception_handlers import api_exception_handler
@@ -41,13 +42,17 @@ async def predict(image: UploadFile, crop_name: str = Form(...)):
     if not image_bytes:
         raise EmptyImageException()
 
-    result = prediction_service.predict(image_bytes, crop_name)
-
+    result =  await run_in_threadpool(
+    prediction_service.predict,
+    image_bytes,
+    crop_name,
+)
     return PredictionResponse(
             success=True,
             message="예측 완료.",
             data=PredictionData(**result),
         )
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
